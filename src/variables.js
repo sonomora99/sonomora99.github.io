b5a387fee0c145a8b54b8be7b7e0235bf1f5ece5
cesar <csoriano@unbosque.edu.co>
2022-01-16 00:07:16 -0500

    upload version

--- src/variables.js
+++ src/variables.js
@@ -1,5 +1,7 @@
-export const SOCKET_URL = "http://localhost:5000"
-export const BASE_URL = "http://localhost:5000/api/"
+// export const SOCKET_URL = "http://localhost:5000"
+export const SOCKET_URL = "https://qa-back-spots.herokuapp.com"
+export const BASE_URL = "https://qa-back-spots.herokuapp.com/api/"
+// export const BASE_URL = "http://localhost:5000/api/"
 
 
 export const REACT_APP_GOOGLE_MAPS_API_KEY =  "AIzaSyAiKZMtAMrozBsYFvbxMtUr_d2n5Lzojks"
